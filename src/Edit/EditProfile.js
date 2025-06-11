efdb7779697aa927e6ad5b30c5e851a24b4080f2
KJW-623 <kimjiwoo4056@gmail.com>
2025-06-11 14:00:21 +0900

    구현 페이지 준비

---
+++ src/Edit/EditProfile.js
@@ -0,0 +1,11 @@
+import './EditProfile.css'
+
+
+function EditProfile() {
+    return(
+        <div>
+            <h1>아마도 프로필 편집 페이지가 될 예정</h1>
+        </div>
+    )
+}
+export default EditProfile;
---
+++ src/main/Home.js
@@ -0,0 +1,11 @@
+import './Home.css'
+
+
+function Home() {
+    return(
+        <div>
+            <h1>아마도 메인 페이지가 될 예정</h1>
+        </div>
+    )
+}
+export default Home;
