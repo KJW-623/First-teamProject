55baa119d25f21ccaa99e7cccfc269f36153f4c5
KJW-623 <kimjiwoo4056@gmail.com>
2025-06-11 12:27:56 +0900

    헤더 카테고리 수정

--- src/header/Header.js
+++ src/header/Header.js
@@ -1,11 +1,14 @@
 import { Link, useLocation } from 'react-router-dom';
+import { useState } from 'react';
 import './Header.css';
 
 const Header = () => {
 
     const location = useLocation();
     const shouldHideHeader = location.pathname === '/login' || location.pathname === '/HelpCenter' || location.pathname === '/register'
-                                || location.pathname === '/FoundID' || location.pathname === '/FoundPW';
+        || location.pathname === '/FoundID' || location.pathname === '/FoundPW';
+
+    const [isCateOpen, setIsCateOpen] = useState(false);
 
     if (shouldHideHeader) {
         return null;
@@ -29,15 +32,15 @@ const Header = () => {
                     </button>
                     <div className="profile-dropdown">
                         <Link to="/MyPage" className="action-link">
-                        <button className="profile-button">
-                            <div className="profile-image-container">
-                                <img
-                                    src="https://cdn.pixabay.com/photo/2022/07/18/19/57/dog-7330712_640.jpg"
-                                    alt="프로필 사진"
-                                    className="profile-image"
-                                />
-                            </div>FundFlow
-                        </button></Link>
+                            <button className="profile-button">
+                                <div className="profile-image-container">
+                                    <img
+                                        src="https://cdn.pixabay.com/photo/2022/07/18/19/57/dog-7330712_640.jpg"
+                                        alt="프로필 사진"
+                                        className="profile-image"
+                                    />
+                                </div>FundFlow
+                            </button></Link>
                     </div>
                 </div>
             </div>
@@ -46,14 +49,33 @@ const Header = () => {
                 <ul className="nav-links">
                     <li>
                         <button className="category-button">
-                            <i className="fas"></i>
-                            카테고리
+                            <i className="fas" onClick={() => setIsCateOpen(!isCateOpen)}>카테고리</i>
                         </button>
+                        {isCateOpen && (
+                    <div className="cate-dropdown">
+                        <ul>
+                            <li>전체</li>
+                            <li>디자인 문구</li>
+                            <li>푸드</li>
+                            <li>영화ㆍ비디오</li>
+                            <li>디지털 게임</li>
+                            <li>홈ㆍ리빙</li>
+                            <li>의류</li>
+                            <li>예술</li>
+                            <li>테크ㆍ가전</li>
+                            <li>잡화</li>
+                            <li>사진</li>
+                            <li>반려동물</li>
+                            <li>주얼리</li>
+                            <li>음악</li>
+                        </ul>
+                    </div>
+                )}
                     </li>
                     <li><Link to="/home">홈</Link></li>
                     <li><Link to="/popular">인기</Link></li>
                     <li><Link to="/open-soon">공개예정</Link></li>
-                    <li><Link to="/RecommandProjects">에디터의 추천</Link></li>
+                    <li><Link to="/RecommandProjects">MD 추천</Link></li>
                 </ul>
                 <div className="search-bar">
                     <input type="text" placeholder="검색어를 입력해주세요." />
