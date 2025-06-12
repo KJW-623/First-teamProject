48a8037bf462edd575f659b10f7dea9a95645331
KJW-623 <kimjiwoo4056@gmail.com>
2025-06-12 14:47:39 +0900

    리뷰 주석 처리

--- src/review/ReviewHeader.js
+++ src/review/ReviewHeader.js
@@ -1,8 +1,9 @@
-
+//창작자 정보
 function ReviewHeader({ creatorInfo }) {
     return (
         <div className="creator-profile-header">
             <div className="profile-summary">
+                    {/* 창작자 이미지 */}
                 <img src={creatorInfo.avatar} className="profile-avatar" />
                 <div className="profile-text">
                     <span className="profile-name">{creatorInfo.name}</span>
--- src/review/ReviewList.js
+++ src/review/ReviewList.js
@@ -1,6 +1,7 @@
 import ReviewCard from "./ReviewCard";
 
 function ReviewList({ reviews }) {
+    //리뷰 정보가 없는 경우
     if (reviews.length === 0) {
         return <div className="no-reviews">표시할 후기가 없습니다.</div>;
     }
